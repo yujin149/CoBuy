4a4236ce33fa5648785ba67bb8404e47f8657d68
yujin <swtask149@gmail.com>
2025-02-06 09:44:57 +0900

    project setting

---
+++ src/main/java/com/cobuy/CoBuyApplication.java
@@ -0,0 +1,13 @@
+package com.cobuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoBuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoBuyApplication.class, args);
+	}
+
+}
