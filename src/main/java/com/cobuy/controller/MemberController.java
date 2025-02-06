671b5c4334b1242e81d2530f094272b76226c976
yujin <swtask149@gmail.com>
2025-02-07 01:40:46 +0900

    사용자P 퍼블리싱 - 로그인 수정 & 아이디찾기 진행중

---
+++ src/main/java/com/cobuy/controller/MemberController.java
@@ -0,0 +1,42 @@
+package com.cobuy.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    /*업체 아이디 찾기*/
+    @GetMapping(value = "/admin/find")
+    public String findAdmin() {
+        return "admin/member/findAdmin";
+    }
+    /*셀러 아이디 찾기*/
+    @GetMapping(value = "/admin/seller/find")
+    public String findSeller() {
+        return "admin/member/findAdmin";
+    }
+    /*개인 아이디 찾기*/
+    @GetMapping(value = "/find")
+    public String findUser() {
+        return "member/findMember";
+    }
+
+    /*업체 회원가입*/
+    @GetMapping(value = "/admin/join")
+    public String joinAdmin() {
+        return "admin/member/joinAdmin";
+    }
+    /*셀러 아이디 찾기*/
+    @GetMapping(value = "/admin/seller/join")
+    public String joinSeller() {
+        return "admin/member/joinAdmin";
+    }
+    /*개인 아이디 찾기*/
+    @GetMapping(value = "/join")
+    public String joinUser() {
+        return "member/joinMember";
+    }
+
+}
