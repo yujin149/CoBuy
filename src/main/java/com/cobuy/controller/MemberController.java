68fdb04d43ff10c7f7fbcb369bf8a7078c7deb7d
yujin <swtask149@gmail.com>
2025-02-07 21:05:02 +0900

    사용자P 퍼블리싱 - 아이디 및 비밀번호 찾기

--- src/main/java/com/cobuy/controller/MemberController.java
+++ src/main/java/com/cobuy/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.cobuy.controller;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,27 +24,33 @@ public class MemberController {
         return "member/findMember";
     }
 
+    /*아이디 찾기 완료*/
     @GetMapping(value = "/findId")
     public String findId() {
         return "member/findId";
     }
 
+    /*비밀번호 재설정*/
     @GetMapping(value = "/findPw")
     public String findPw() {
         return "member/findPw";
     }
+
+    /*비밀번호 변경 완료*/
+    @GetMapping(value = "/updatePw")
+    public String updatePw() {return "member/updatePw";}
 
     /*업체 회원가입*/
     @GetMapping(value = "/admin/join")
     public String joinAdmin() {
         return "admin/member/joinAdmin";
     }
-    /*셀러 아이디 찾기*/
+    /*셀러 회원가입*/
     @GetMapping(value = "/admin/seller/join")
     public String joinSeller() {
         return "admin/member/joinAdmin";
     }
-    /*개인 아이디 찾기*/
+    /*개인 회원가입*/
     @GetMapping(value = "/join")
     public String joinUser() {
         return "member/joinMember";
