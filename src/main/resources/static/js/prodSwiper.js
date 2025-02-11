67ac67a4f2aae104c854c8aef15020dab85accda
yujin <swtask149@gmail.com>
2025-02-11 19:23:27 +0900

    사용자P 상품 페이지 퍼블리싱

---
+++ src/main/resources/static/js/prodSwiper.js
@@ -0,0 +1,8 @@
+$(function(){
+    var swiper = new Swiper(".prodImgSlide", {
+        pagination: {
+            el: ".swiper-pagination",
+            dynamicBullets: true,
+        },
+    });
+});
