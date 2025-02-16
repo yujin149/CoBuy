37859635bdb9c549cc5e37f44d87af4aeb45ed00
yujin149 <swtask149@gmail.com>
2025-02-17 03:09:01 +0900

    사용자P 장바구니 페이지 완료

--- src/main/resources/static/js/product.js
+++ src/main/resources/static/js/product.js
@@ -1,22 +1,22 @@
 $(function(){
     // 증가 버튼 클릭
     $('.countBox .plus').click(function() {
-        let currentValue = $('#quantity').val();
-        $('#quantity').val(parseInt(currentValue) + 1);
+        let currentValue = $('.countBox input').val();
+        $('.countBox input').val(parseInt(currentValue) + 1);
     });
 
     // 감소 버튼 클릭
     $('.countBox .minus').click(function() {
-        let currentValue = $('#quantity').val();
+        let currentValue = $('.countBox input').val();
         if (currentValue > 1) {
-            $('#quantity').val(parseInt(currentValue) - 1);
+            $('.countBox input').val(parseInt(currentValue) - 1);
         } else {
             alert("최소 구매 수량은 1개입니다.");
         }
     });
 
     // 직접 입력 시 0 이하의 값이 들어가면 경고
-    $('#quantity').on('input', function() {
+    $('.countBox input').on('input', function() {
         if ($(this).val() < 1) {
             alert("최소 구매 수량은 1개입니다.");
             $(this).val(1);
