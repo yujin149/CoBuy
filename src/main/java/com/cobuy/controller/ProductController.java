77cc4748de37c939939676746e2476e8d3472c53
yujin149 <swtask149@gmail.com>
2025-02-16 23:41:57 +0900

    이름변경

--- src/main/java/com/cobuy/controller/ProductController.java
+++ src/main/java/com/cobuy/controller/ProductController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
-public class productController {
+public class ProductController {
     /*상품 리스트 페이지*/
     @GetMapping(value = "/product")
     public String productList(Model model) {
