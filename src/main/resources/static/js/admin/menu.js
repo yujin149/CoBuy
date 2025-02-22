e8d0275fcd71491c5413aeaa1a51e03fa2b624ad
yujin149 <swtask149@gmail.com>
2025-02-22 23:00:10 +0900

    환경설정P 퍼블리싱 완료 / 서브메뉴 없을 시 페이지 이동 script 수정

--- src/main/resources/static/js/admin/menu.js
+++ src/main/resources/static/js/admin/menu.js
@@ -1,14 +1,17 @@
-$(function(){
+$(function() {
     $(".adminMenuWrap .depth2").hide();
     $(".adminMenuWrap .menuWrap > li > a").click(function(e) {
-        e.preventDefault();
         let $parentLi = $(this).parent("li");
 
-        $(".adminMenuWrap .menuWrap > li").removeClass("active");
-        $parentLi.addClass("active");
+        // 서브 메뉴가 있는 경우에만 슬라이드 효과를 적용
+        if ($parentLi.children(".depth2").length > 0) {
+            e.preventDefault();
 
-        $(".adminMenuWrap .depth2").stop().slideUp();
+            $(".adminMenuWrap .menuWrap > li").removeClass("active");
+            $parentLi.addClass("active");
 
-        $parentLi.children(".depth2").stop().slideDown();
+            $(".adminMenuWrap .depth2").stop().slideUp();
+            $parentLi.children(".depth2").stop().slideDown();
+        }
     });
 });
